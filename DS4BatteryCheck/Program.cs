781bae643fc2b579ee727317c036ad6f19a4349a
Temetra <temetra.xy+git@gmail.com>
2020-11-06 14:35:16 +0000

    Added basic console app for checking battery

---
+++ DS4BatteryCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DualShock4Lib;
+
+namespace DS4BatteryCheck
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Checking DS4 battery levels...");
+
+			IControllersProvider controllers = new Controllers();
+			
+			foreach (var controller in controllers.GetAllControllers())
+			{
+				IBatteryState battery = controller.GetBatteryState();
+				
+				if (battery != null)
+				{
+					Console.WriteLine($"> Battery: {battery.Level}% State: {battery.ChargingState}");
+				}
+			}
+
+			Console.WriteLine($"Press any key to exit");
+			Console.ReadKey(true);
+		}
+	}
+}
